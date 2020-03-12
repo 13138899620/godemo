be9cff036777efca2dce0f43774ac539e5e3133f
tea <010@lydjy.net>
2020-03-13 00:24:34 +0800

    test

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello World!!! ")
+		c.String(http.StatusOK, "Hello World!! ")
 	})
 	router.Run(":8000")
 }
